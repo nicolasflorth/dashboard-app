4e4b2b9c437a8165ab98aca90b5009527d0b807c
Nicolae Florescu <nicolaeflorescu.info@yahoo.com>
2025-07-02 16:55:24 +0100

    remove uneeded file left behind

--- dashboard-frontend/src/services/budget.ts
+++
@@ -1,6 +0,0 @@
-import axiosInstance from './axiosInstance';
-
-export const getBudgets = async () => {
-  const response = await axiosInstance.get('/budgets'); // example endpoint
-  return response.data;
-};
